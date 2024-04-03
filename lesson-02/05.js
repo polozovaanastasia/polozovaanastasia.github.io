ed7f90bf1712f3b038c28c0e3945fca1b658b8b1
polozovaanastasia <polozova.anastasia@gmail.com>
2024-04-03 06:20:23 +0500

    tasks 1-5.

---
+++ lesson-02/05.js
@@ -0,0 +1,8 @@
+const passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+const passport2 = {...passport, name: 'Ivan'}
+
+console.log(passport);
+console.log(passport2);
---
+++ lesson-02/06.js
@@ -0,0 +1,8 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
