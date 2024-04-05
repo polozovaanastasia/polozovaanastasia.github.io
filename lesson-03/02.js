0d26bf68e47a5ae871ce1f13c4837d6afd4b84d3
polozovaanastasia <polozova.anastasia@gmail.com>
2024-04-06 01:36:42 +0500

    week 3. task 3.

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,7 +1,5 @@
 let words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 
-const search = array => {
-    return array.indexOf('апельсин') < 0 ? 'Придется поискать в другом магазине…' : 'Ура! нашел'
-}
+const search = array => array.indexOf('апельсин') < 0 ? 'Придется поискать в другом магазине…' : 'Ура! нашел'
 
 console.log(search(words));
