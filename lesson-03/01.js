630e9e2ba032100d604bf08cd2a827f53998fa49
polozovaanastasia <polozova.anastasia@gmail.com>
2024-04-06 01:07:30 +0500

    week 3. task 1.

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+let colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString(array) {
+    return colors.filter( item => (item === 'черный' || item === 'красный' || item === 'желтый')).join('-');
+}
+
+console.log(createColorString(colors));
